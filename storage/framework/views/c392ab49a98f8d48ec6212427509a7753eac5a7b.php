d9ba035c19f9b14bd91b224076dbbc58c60e30cd
Saurav Wagh <sauravwagh2079@gmail.com>
2025-05-05 18:48:09 +0530

    views file added

---
+++ storage/framework/views/c392ab49a98f8d48ec6212427509a7753eac5a7b.php
@@ -0,0 +1,56 @@
+<!-- Main Content-->
+            <div class="main-content side-content pt-0">
+                <!-- Main Header-->
+                <div class="main-header side-header sticky" style="background-color: #C3B1E1;">
+                    <div class="container-fluid">
+                        <div class="main-header-left">
+                            <a class="main-logo d-lg-none" href="https://laravel.spruko.com/dashlead/Leftmenu-Icon-Light/index">
+                                <img src="https://laravel.spruko.com/dashlead/Leftmenu-Icon-Light/assets/img/brand/logo.png" class="header-brand-img desktop-logo" alt="logo">
+                                <img src="https://laravel.spruko.com/dashlead/Leftmenu-Icon-Light/assets/img/brand/icon.png" class="header-brand-img icon-logo" alt="logo">
+                                <img src="https://laravel.spruko.com/dashlead/Leftmenu-Icon-Light/assets/img/brand/logo-light.png" class="header-brand-img desktop-logo theme-logo" alt="logo">
+                                <img src="https://laravel.spruko.com/dashlead/Leftmenu-Icon-Light/assets/img/brand/icon-light.png" class="header-brand-img icon-logo theme-logo" alt="logo">
+                            </a>
+                            <a class="main-header-menu-icon" href="" id="mainSidebarToggle"><span></span></a>
+                        </div>
+                        <div class="mr-auto float-left bookmark-wrapper d-flex align-items-center">
+
+                </div>               
+             
+                        <div class="main-header-right">
+                            
+
+                                <ul class="nav navbar-nav float-right">
+
+                    <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-expand" ><i class="ficon feather icon-maximize"></i></a></li>
+
+                    <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown" style=" margin-top: -12px;">
+                        <div class="user-nav d-sm-flex d-none"><span style="margin: auto;"><img class="round center" src="<?php echo e(config('app.baseURL')); ?>/admin-assets/images/user.png" alt="avatar" height="40" width="40"></span><span style="font-size: 15px; font-weight: 600;color: white;">admin</span>
+                        </div>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" style="background-color: #C3B1E1;">
+                        <!-- <a class="dropdown-item"><i class="feather icon-power"></i>
+                            
+                        </a> -->
+                       <a class="dropdown-item" href="<?php echo e(config('app.baseURL')); ?>/logout" style="    background-color: #C3B1E1; border-radius: 40px; border-color: #C3B1E1;"><i class="fe fe-power" style="margin: 5px; color: white;"></i><span style="    color: white;">Logout</span></a>
+                        <form id="logout-form" action="login" method="POST" style="display: none; ">
+                            <input type="hidden" name="_token" value="GdyPYOKZAR3FMLesHiRNpzvZDkKB9Th0na5rBXoI">                            </form>
+                        </div>
+                    </li>
+                </ul>
+                           
+
+                    
+                          
+                            <div class="dropdown d-md-flex header-settings">
+                                <!-- <a href="#" class="nav-link icon" data-toggle="sidebar-right" data-target=".sidebar-right">
+                                    <i class="fe fe-align-right"></i>
+                                </a> -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+<?php /**PATH D:\auction-comfyhomes\resources\views/admin layouts/header.blade.php ENDPATH**/ ?>
